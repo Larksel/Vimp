0ad99373a936a509d3aed170a76fadb99f2b5674
Larksel <lemuelgames@outlook.com>
2022-07-25 20:13:20 -0300

    added the interface to the main script

--- ProjectVimp.py
+++ ProjectVimp.py
@@ -1,39 +1,81 @@
 from pytube import YouTube
-from sys import argv
 from moviepy.editor import *
 import os
+from tkinter import *
 
-# Download the video to a temp file
+class Application:
+    def __init__(self, master=None):
+        self.fontePadrao = ("Arial", 10)
 
-link = argv[1]
-yt = YouTube(link)
+        # Titulo
+        self.topo = Frame(master)
+        self.topo["pady"] = 20
+        self.topo.pack(fill="x")
 
-print(f"Downloading: {yt.title}")
+        self.titulo = Label(self.topo, text="Project Vimp")
+        self.titulo["font"] = ("Aller Bold", 28)
+        self.titulo.pack()
 
-video = yt.streams.get_lowest_resolution()
-video.download("C:/Users/Lemuel/Documents/Vimp Temp")
+        # Link
+        self.link = Frame(master)
+        self.link.pack(expand=1)
+        
+        self.linkcontainer = Frame(self.link)
+        self.linkcontainer.pack()
 
-# Convert to mp3
+        self.linkLabel = Label(self.linkcontainer, text="Link:", font=self.fontePadrao)
+        self.linkLabel.pack(side="left")
 
-def FormatFilename(filename):
-  blacklist = set(".',|:\/")
-  for ch in filename:
-    if ch in blacklist:
-      filename = filename.replace(ch, "")
-  return filename
+        self.linkbox = Entry(self.linkcontainer, font=self.fontePadrao)
+        self.linkbox["width"] = 60
+        self.linkbox.pack(side="left")
 
-mp4_file = "C:/Users/Lemuel/Documents/Vimp Temp/" + FormatFilename(f"{yt.title}") + ".mp4"
-mp3_file = "C:/Users/Lemuel/Desktop/Music/Vimp Music/" + FormatFilename(f"{yt.title}") + ".mp3"
+        # botao
+        self.botaocontainer = Frame(self.link)
+        self.botaocontainer["pady"] = 10
+        self.botaocontainer.pack(side="bottom", fill="x")
 
-videoclip = VideoFileClip(mp4_file)
-audioclip = videoclip.audio
-audioclip.write_audiofile(mp3_file)
-audioclip.close()
-videoclip.close()
+        self.botaolink = Button(self.botaocontainer, font=self.fontePadrao)
+        self.botaolink["text"] = "Baixar"
+        self.botaolink["command"] = self.baixar
+        self.botaolink.pack()
 
-# Remove temp files
+    def baixar(self):
+        link = self.linkbox.get()
+        yt = YouTube(link)
 
-if os.path.exists(mp4_file):
-  os.remove(mp4_file)
-else:
-  print("The file does not exist")
+        print(f"Downloading: {yt.title}")
+
+        # Download the video to a temp file
+        
+        video = yt.streams.get_lowest_resolution()
+        video.download("C:/Users/Lemuel/Documents/Vimp Temp")
+
+        # Convert to mp3
+
+        def FormatFilename(filename):
+            blacklist = set(".',|:\/")
+            for ch in filename:
+                if ch in blacklist:
+                    filename = filename.replace(ch, "")
+            return filename
+
+        mp4_file = "C:/Users/Lemuel/Documents/Vimp Temp/" + FormatFilename(f"{yt.title}") + ".mp4"
+        mp3_file = "C:/Users/Lemuel/Desktop/Music/Vimp Music/" + FormatFilename(f"{yt.title}") + ".mp3"
+
+        videoclip = VideoFileClip(mp4_file)
+        audioclip = videoclip.audio
+        audioclip.write_audiofile(mp3_file)
+        audioclip.close()
+        videoclip.close()
+
+        # Remove temp files
+
+        if os.path.exists(mp4_file):
+            os.remove(mp4_file)
+        else:
+            print("The file does not exist")
+
+root = Tk()
+Application(root)
+root.mainloop()
