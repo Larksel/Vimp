89261c1ebbd4426719785190636a0ea4f2c64dae
Larksel <lemuelgames@outlook.com>
2022-11-04 02:24:42 -0300

    Added more special characters to blacklist

--- GerenciaDiretorio.py
+++ GerenciaDiretorio.py
@@ -4,8 +4,9 @@ def verificaDiretorio(dir):
     if not os.path.exists(dir):
         os.mkdir(dir)
 
+# Function to remove every non supported character from the filename
 def formatFilename(filename):
-    blacklist = set(".',|:\/" + '"')
+    blacklist = set(".'?*,<;>|:\/" + '"')
     for ch in filename:
         if ch in blacklist:
             filename = filename.replace(ch, "")
