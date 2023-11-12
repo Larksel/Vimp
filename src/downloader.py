a874d10d02dac81832f6d3652ff60639fe23ffa7
Larksel <lemuelgames@outlook.com>
2023-11-12 01:53:08 -0300

    Limpeza de código

--- src/downloader.py
+++ src/downloader.py
@@ -11,8 +11,7 @@ if __name__ != "__main__":
 
 
 class Downloader:
-    def __init__(self, link):
-
+    def __init__(self, link) -> None:
         # Carrega todos os atributos necessarios
         self.link = link
 
@@ -42,9 +41,13 @@ class Downloader:
     # Downloads the video's thumbnail image
     def get_thumbnail(self) -> None:
         opener = urllib.request.build_opener()
-        opener.addheaders = [("User-Agent",
-                              "Mozilla/5.0 (Windows NT 6.1; WOW64) " +
-                              "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36")]
+        opener.addheaders = [
+            (
+                "User-Agent",
+                "Mozilla/5.0 (Windows NT 6.1; WOW64) "
+                + "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36",
+            )
+        ]
         urllib.request.install_opener(opener)
 
         image_url = self.thumbnail_url
@@ -76,6 +79,7 @@ class Downloader:
 
         mp4_file = dirmanager.tempfolder + f"{self.formatted_title}.mp4"
         mp3_file = dirmanager.musicfolder + f"{self.formatted_title}.mp3"
+        
         Converter.video_to_audio(mp4_file, mp3_file)
         FileManager.set_audio_metadata(mp3_file, self.title, self.thumbnail_path)
 
@@ -106,4 +110,3 @@ if __name__ == "__main__":
     dirmanager = DirManager()
 
     # Test code goes here
-    
