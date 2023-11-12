0d5be26d77123f2aea0d467ab04e2d54a7b4dbcb
Larksel <lemuelgames@outlook.com>
2023-11-12 01:54:09 -0300

    Script para alterar a arte do album de um mp3

---
+++ alterar_arte.py
@@ -0,0 +1,32 @@
+from src.downloader import Downloader
+from src.managers import FileManager
+from pathlib import Path
+
+
+def main():
+    while True:
+        url = input("Link do vídeo \n -> ")
+
+        if url != '':
+            downloader = Downloader(url)
+            print(downloader.title)
+        else:
+            print('Nenhum valor inserido')
+            break
+        
+        audiofile = input("Arquivo a ser alterado \n -> ").replace('"', '')
+
+        if audiofile != "":
+            titulo = Path(audiofile).stem
+            
+            downloader.get_thumbnail()
+
+            FileManager.set_audio_metadata(
+                audiofile=audiofile,
+                thumbimg=downloader.thumbnail_path,
+                titulo=titulo
+            )
+        else:
+            print('Valor inválido')
+
+main()
