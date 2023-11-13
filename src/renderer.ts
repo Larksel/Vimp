9b51db96a727eca8156603a456cc39f62653e317
Larksel <lemuelgames@outlook.com>
2023-11-13 02:17:25 -0300

    Alguns ajustes

--- src/renderer.ts
+++ src/renderer.ts
@@ -4,29 +4,9 @@
  * Electron, visit:
  *
  * https://electronjs.org/docs/latest/tutorial/process-model
- *
- * By default, Node.js integration in this file is disabled. When enabling Node.js integration
- * in a renderer process, please be aware of potential security implications. You can read
- * more about security risks here:
- *
- * https://electronjs.org/docs/tutorial/security
- *
- * To enable Node.js integration in this file, open up `main.js` and enable the `nodeIntegration`
- * flag:
- *
- * ```
- *  // Create the browser window.
- *  mainWindow = new BrowserWindow({
- *    width: 800,
- *    height: 600,
- *    webPreferences: {
- *      nodeIntegration: true
- *    }
- *  });
- * ```
  */
 
-import './index.css';
-import './app.tsx';
+import './main.css';
+import './index.tsx';
 
 console.log('👋 This message is being logged by "renderer.js", included via webpack');
