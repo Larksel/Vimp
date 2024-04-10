36b9033c598a24519bd5a5b12b82f3fc57ccf4af
Larksel <lemuelgames@outlook.com>
2024-04-10 14:37:51 -0300

    :lipstick: feat: Estilo da barra de scroll vertical

--- app/src/componentes/ScrollBar/ScrollBar.tsx
+++ app/src/componentes/ScrollBar/ScrollBar.tsx
@@ -2,11 +2,20 @@ import * as ScrollArea from '@radix-ui/react-scroll-area';
 
 export default function ScrollBar({ children }) {
   return (
-    <ScrollArea.Root className='h-full w-full'>
-      <ScrollArea.Viewport className='relative h-full bg-lime-500'>
+    <ScrollArea.Root className='h-full w-full overflow-clip'>
+      <ScrollArea.Viewport className='relative w-full h-full'>
         {children}
       </ScrollArea.Viewport>
-      <ScrollArea.Scrollbar orientation='vertical'>
+      <ScrollArea.Scrollbar
+        className='w-2 h-full'
+        orientation='vertical'
+      >
+        <ScrollArea.Thumb className='w-2 bg-neutral-200 opacity-30' />
+      </ScrollArea.Scrollbar>
+      <ScrollArea.Scrollbar 
+        className='w-full h-2'
+        orientation='horizontal'
+      >
         <ScrollArea.Thumb />
       </ScrollArea.Scrollbar>
       <ScrollArea.Corner />
