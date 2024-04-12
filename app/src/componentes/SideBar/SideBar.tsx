48e24ab022da4664fa1d9846fa3c15b432c7c1c3
Larksel <lemuelgames@outlook.com>
2024-04-12 16:12:40 -0300

    :lipstick: feat: Layout principal em grid

--- app/src/componentes/SideBar/SideBar.tsx
+++ app/src/componentes/SideBar/SideBar.tsx
@@ -1,9 +1,5 @@
 import { useState } from 'react';
 
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-
 import ChevronLeftRoundedIcon from '@mui/icons-material/ChevronLeftRounded';
 
 import logo from '../../assets/images/logo.png';
@@ -11,12 +7,10 @@ import logo from '../../assets/images/logo.png';
 import NavButtons from './NavButtons';
 import PlaylistList from './PlaylistList';
 
-export default function SideBar() {
-  const [collapsed, setCollapsed] = useState(false);
-
+export default function SideBar({ toggle, collapsed }) {
   return (
-    <div className={`w-full ${collapsed ? 'max-w-16' : 'max-w-80'} flex flex-col overflow-clip transition-all shrink-0`}>
-      <button onClick={() => setCollapsed(!collapsed)}>
+    <div className={`w-full ${collapsed ? 'max-w-20' : 'max-w-80'} flex flex-col overflow-clip transition-all shrink-0`}>
+      <button onClick={toggle}>
         Toggle Menu
       </button>
 
--- app/src/core/App.tsx
+++ app/src/core/App.tsx
@@ -7,32 +7,26 @@ import Rotas from './Rotas';
 import PlaybackConsole from '../componentes/PlaybackConsole/PlaybackConsole';
 
 import ScrollBar from '../componentes/ScrollBar/ScrollBar';
+import { useState } from 'react';
 
 export default function App() {
+  const [collapsed, setCollapsed] = useState(false);
+
   return (
     <HashRouter>
-      {
-        //TODO testar grid
-      }
-      <div className='flex h-screen w-screen flex-col overflow-hidden bg-black'>
+      <div className='grid h-screen w-full grid-cols-1 grid-rows-[36px,auto,96px] overflow-hidden bg-black'>
         <AppBar />
         <div
-          className='flex flex-1 flex-row overflow-clip bg-red-500'
+          className={`grid grid-rows-1 ${collapsed ? 'grid-cols-[80px,auto]' : 'grid-cols-[320px,auto]'} overflow-clip bg-red-500 transition-all`}
           style={{
             maxHeight: 'calc(100vh - 36px - 96px)',
           }}
         >
-          <SideBar />
-          <div
-            className='relative flex flex-1 basis-0'
-            style={{
-              maxWidth: 'calc(100vw - 320px)',
-            }}
-          >
-            {
-              // ! Revisar estrutura DOM e definir quais elementos
-              // ! dão overflow em quem
-            }
+          <SideBar
+            collapsed={collapsed}
+            toggle={() => setCollapsed(!collapsed)}
+          />
+          <div className='relative'>
             <Header />
             <ScrollBar>
               <Rotas />
