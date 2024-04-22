57647d52b77bbf1023dade74d1e54e41196850a6
Larksel <lemuelgames@outlook.com>
2024-04-22 01:53:38 -0300

    :lipstick: feat: Estilo do ExpandedView retrabalhado

--- app/src/componentes/PlaybackConsole/ExpandedView.tsx
+++ app/src/componentes/PlaybackConsole/ExpandedView.tsx
@@ -2,8 +2,6 @@ import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { selectCurrentTrack } from '../../features/playerSlice';
 
-import Box from '@mui/material/Box';
-
 import placeholder from '../../assets/images/placeholder.png';
 
 interface ExpandedViewProps {
@@ -19,46 +17,17 @@ export default function ExpandedView({ visible }: ExpandedViewProps) {
   }, [track.cover]);
 
   return (
-    <Box
-      sx={{
-        opacity: visible ? 1 : 0,
-        position: 'absolute',
-        top: visible ? 0 : '100%',
-        right: 0,
-        left: 0,
-        bottom: '88px',
-        backgroundImage: `url(${cover || placeholder})`,
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'center',
-        backgroundSize: 'cover',
-        transition: 'opacity .15s ease',
-      }}
+    <div
+      className={`${visible ? 'top-0 opacity-100' : 'top-full opacity-0'} absolute bottom-[88px] left-0 right-0 transition-all`}
     >
-      <Box
-        sx={{
-          height: '100%',
-          width: '100%',
-          bgcolor: '#222c',
-          backdropFilter: 'blur(10px)',
-        }}
-      />
-      <Box
-        component='img'
+      <img
         src={cover || placeholder}
-        sx={{
-          position: 'absolute',
-          bottom: visible ? '16px' : 0,
-          left: '16px',
-          height: '100%',
-          width: '100%',
-          maxHeight: '256px',
-          maxWidth: '256px',
-          borderRadius: '8px',
-          boxShadow: '0 5px 15px #0007',
-          objectFit: 'cover',
-          transition: 'all .15s ease',
-        }}
+        className='absolute inset-0 size-full object-cover blur'
       />
-    </Box>
+      <img
+        src={cover || placeholder}
+        className={`absolute left-4 size-64 rounded-lg border border-black/30 object-cover shadow-md transition-all ${visible ? 'visible bottom-4' : 'invisible bottom-0'}`}
+      />
+    </div>
   );
 }
