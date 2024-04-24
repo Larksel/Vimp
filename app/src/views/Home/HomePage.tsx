1b148bbe4bcf391506f9200bbe531abafa57d36d
Larksel <lemuelgames@outlook.com>
2024-04-24 16:07:45 -0300

    HomePage preparada com placeholders

--- app/src/views/Home/HomePage.tsx
+++ app/src/views/Home/HomePage.tsx
@@ -1,9 +1,87 @@
-// CardList
+import CardList from '@/componentes/CardList/CardList';
+import { Track } from '../../../shared/types/vimp';
 
+const exemplo: Track[] = [
+  {
+    title: 'titulo',
+    album: 'album',
+    artist: ['artista'],
+    genre: ['genero'],
+    duration: 375,
+    playCount: 0,
+    favorite: false,
+    lastPlayed: null,
+    path: '',
+    cover: '',
+  },
+  {
+    title: 'titulo',
+    album: 'album',
+    artist: ['artista'],
+    genre: ['genero'],
+    duration: 375,
+    playCount: 0,
+    favorite: false,
+    lastPlayed: null,
+    path: '',
+    cover: '',
+  },
+  {
+    title: 'titulo',
+    album: 'album',
+    artist: ['artista'],
+    genre: ['genero'],
+    duration: 375,
+    playCount: 0,
+    favorite: false,
+    lastPlayed: null,
+    path: '',
+    cover: '',
+  },
+  {
+    title: 'titulo',
+    album: 'album',
+    artist: ['artista'],
+    genre: ['genero'],
+    duration: 375,
+    playCount: 0,
+    favorite: false,
+    lastPlayed: null,
+    path: '',
+    cover: '',
+  },
+  {
+    title: 'titulo',
+    album: 'album',
+    artist: ['artista'],
+    genre: ['genero'],
+    duration: 375,
+    playCount: 0,
+    favorite: false,
+    lastPlayed: null,
+    path: '',
+    cover: '',
+  },
+  {
+    title: 'titulo',
+    album: 'album',
+    artist: ['artista'],
+    genre: ['genero'],
+    duration: 375,
+    playCount: 0,
+    favorite: false,
+    lastPlayed: null,
+    path: '',
+    cover: '',
+  },
+]
 export default function HomePage() {
   return (
     <div>
-      Home
+      <h1>Home</h1>
+      <CardList data={exemplo}/>
+      <CardList data={exemplo}/>
+      <CardList data={exemplo}/>
     </div>
   );
 }
