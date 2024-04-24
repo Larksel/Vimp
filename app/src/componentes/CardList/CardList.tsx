890d2b7d5a2ad613182a8314f841e012acae2684
Larksel <lemuelgames@outlook.com>
2024-04-24 16:31:58 -0300

    :lipstick: feat: Estilo do CardList passado para tailwind

--- app/src/componentes/CardList/CardList.tsx
+++ app/src/componentes/CardList/CardList.tsx
@@ -11,35 +11,13 @@ type CardListProps = {
 //TODO Grid
 export default function CardList({ data }: CardListProps) {
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        flexDirection: 'row',
-        gap: '24px',
-        paddingY: '12px',
-        height: '100%',
-        width: '100%',
-        overflowY: 'hidden',
-        overflowX: 'auto',
-        '&::-webkit-scrollbar': {
-          height: '4px',
-        },
-        '&::-webkit-scrollbar-thumb': {
-          backgroundColor: '#5A5A5A',
-          borderRadius: '8px',
-        },
-        '&::-webkit-scrollbar-track': {
-          backgroundColor: '#0000',
-          borderRadius: '8px',
-        },
-      }}
-    >
+    <div className='flex gap-6 py-3 h-full w-full overflow-auto'>
       {data.map((item, index) => (
         <MediaCard
           key={index}
           item={item}
         />
       ))}
-    </Box>
+    </div>
   );
 }
