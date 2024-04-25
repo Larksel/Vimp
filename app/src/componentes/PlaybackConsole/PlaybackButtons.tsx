240739cc8b5b68932a93b73621ad4846a3a3c152
Larksel <lemuelgames@outlook.com>
2024-04-25 01:10:13 -0300

    :lipstick: feat: tamanho e espaçamento do PlaybackButtons

--- app/src/componentes/PlaybackConsole/PlaybackButtons.tsx
+++ app/src/componentes/PlaybackConsole/PlaybackButtons.tsx
@@ -35,50 +35,50 @@ export default function PlaybackButtons() {
   };
 
   const repeatIcons = {
-    off: <Repeat size={24} />,
-    all: <Repeat size={24} className='text-green-500' />,
-    one: <RepeatOnce size={24} className='text-green-500' />,
+    off: <Repeat size={20} />,
+    all: <Repeat size={20} className='text-green-500' />,
+    one: <RepeatOnce size={20} className='text-green-500' />,
   };
 
   return (
     <div className='flex w-full items-center gap-4'>
-      <div className='flex flex-1 justify-end gap-4'>
+      <div className='flex flex-1 justify-end gap-2'>
         <button
-          className='text-zinc-400 transition-colors hover:text-zinc-100'
+          className='relative flex size-8 items-center justify-center text-zinc-400 transition-colors hover:text-zinc-100'
           onClick={() => dispatch(setShuffle(!shuffle))}
         >
-          <Shuffle size={24} className={`${shuffle ? 'text-green-500' : ''}`} />
+          <Shuffle size={20} className={`${shuffle ? 'text-green-500' : ''}`} />
         </button>
 
         <button
-          className='text-zinc-400 transition-colors hover:text-zinc-100'
+          className='relative flex size-8 items-center justify-center text-zinc-400 transition-colors hover:text-zinc-100'
           onClick={() => player.previous()}
         >
-          <SkipBack size={24} />
+          <SkipBack size={20} />
         </button>
       </div>
 
       <button
         onClick={handlePlayPause}
-        className='flex h-9 w-9 items-center justify-center rounded-full bg-white text-black'
+        className='flex size-8 items-center justify-center rounded-full bg-white text-black'
       >
         {isPlaying ? (
-          <Pause weight='fill' size={20} />
+          <Pause weight='fill' size={16} />
         ) : (
-          <Play weight='fill' size={20} />
+          <Play weight='fill' size={16} />
         )}
       </button>
 
-      <div className='flex flex-1 gap-4'>
+      <div className='flex flex-1 gap-2'>
         <button
-          className='text-zinc-400 transition-colors hover:text-zinc-100'
+          className='relative flex size-8 items-center justify-center text-zinc-400 transition-colors hover:text-zinc-100'
           onClick={() => player.next()}
         >
-          <SkipForward size={24} />
+          <SkipForward size={20} />
         </button>
 
         <button
-          className='text-zinc-400 transition-colors hover:text-zinc-100'
+          className='relative flex size-8 items-center justify-center text-zinc-400 transition-colors hover:text-zinc-100'
           onClick={() => dispatch(changeRepeat())}
         >
           {repeatIcons[repeat]}
