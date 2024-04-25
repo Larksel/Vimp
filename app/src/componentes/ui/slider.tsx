eab15cf07fdf182cc0d9ef5718a70c7e89a2f3a1
Larksel <lemuelgames@outlook.com>
2024-04-25 00:50:58 -0300

    :lipstick: feat: estilo do slider mais parecido com o antigo

--- app/src/componentes/ui/slider.tsx
+++ app/src/componentes/ui/slider.tsx
@@ -3,8 +3,6 @@ import * as SliderPrimitive from '@radix-ui/react-slider';
 
 import { cn } from '@/lib/utils';
 
-//TODO aplicar estilos antigos
-
 const Slider = React.forwardRef<
   React.ElementRef<typeof SliderPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof SliderPrimitive.Root>
@@ -18,9 +16,9 @@ const Slider = React.forwardRef<
     {...props}
   >
     <SliderPrimitive.Track className='relative h-1 w-full grow overflow-hidden rounded-full bg-white/20'>
-      <SliderPrimitive.Range className='absolute h-full rounded-full bg-red-500' />
+      <SliderPrimitive.Range className='absolute h-full rounded-full bg-white transition-colors group-hover:bg-red-500' />
     </SliderPrimitive.Track>
-    <SliderPrimitive.Thumb className='block size-0 rounded-full bg-white shadow transition-all focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 group-hover:size-4' />
+    <SliderPrimitive.Thumb className='block size-0 rounded-full bg-white shadow focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 group-hover:size-4' />
   </SliderPrimitive.Root>
 ));
 Slider.displayName = SliderPrimitive.Root.displayName;
