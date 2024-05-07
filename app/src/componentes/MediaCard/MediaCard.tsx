be1f223394d69928e3b0f600e1b1dcd314970314
Larksel <lemuelgames@outlook.com>
2024-05-07 01:59:55 -0300

    :lipstick: feat: Cor e tamanho MediaCard

--- app/src/componentes/MediaCard/MediaCard.tsx
+++ app/src/componentes/MediaCard/MediaCard.tsx
@@ -29,7 +29,7 @@ export default function MediaCard({ item }: MediaCardProps) {
   return (
     <Card
       onClick={playTrack}
-      className='group flex h-72 w-52 flex-col space-y-2 overflow-hidden p-4 transition-all duration-300 hover:cursor-pointer hover:bg-neutral-700/70'
+      className='group relative flex h-72 w-56 flex-col space-y-2 overflow-hidden border-neutral-700/50 bg-neutral-800 p-4 transition-all duration-300 hover:cursor-pointer hover:bg-neutral-700/70'
     >
       <CardHeader className='relative space-y-0 p-0'>
         <button className='absolute bottom-0 right-2 flex size-10 items-center justify-center rounded-full bg-green-500 opacity-0 shadow-sm transition-all duration-300 group-hover:bottom-2 group-hover:opacity-100'>
@@ -44,7 +44,7 @@ export default function MediaCard({ item }: MediaCardProps) {
         <InfoText variant='primary'>{item.title}</InfoText>
         <InfoText variant='secondary'>{item.artist}</InfoText>
       </CardContent>
-      <p className='text-right text-sm text-neutral-400'>
+      <p className='absolute bottom-4 right-4 w-fit text-sm text-neutral-400'>
         {formatDuration(item.duration)}
       </p>
     </Card>
