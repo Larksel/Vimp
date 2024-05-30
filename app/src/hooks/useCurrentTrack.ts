0168dd7eba480421520a6381f56a6f2ddc078bd1
Larksel <lemuelgames@outlook.com>
2024-05-30 01:43:29 -0300

    :sparkles: feat: Hook para a track atual

---
+++ app/src/hooks/useCurrentTrack.ts
@@ -0,0 +1,12 @@
+import usePlayerStore from "@/stores/usePlayerStore";
+import { TrackModel } from "../../shared/types/vimp";
+
+export default function useCurrentTrack(): TrackModel | null {
+  return usePlayerStore((state) => {
+    if (state.queue.length > 0 && state.queuePosition !== null) {
+      return state.queue[state.queuePosition];
+    }
+
+    return null;
+  })
+}
