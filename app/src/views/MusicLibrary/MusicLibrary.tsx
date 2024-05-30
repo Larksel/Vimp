18414c3fc18947074d7e270c4cddefc8778fe564
Larksel <lemuelgames@outlook.com>
2024-05-30 04:15:06 -0300

    :recycle: refactor: MusicLibrary atualizada

--- app/src/views/MusicLibrary/MusicLibrary.tsx
+++ app/src/views/MusicLibrary/MusicLibrary.tsx
@@ -4,13 +4,13 @@ import { TrackModel } from '../../../shared/types/vimp';
 import { Button } from '@/componentes/ui/button';
 import MediaCard from '@/componentes/MediaCard/MediaCard';
 
-import queue from '@/lib/queue';
-import player from '@/lib/player';
+import { usePlayerAPI } from '@/stores/usePlayerStore';
 
 //TODO estado da lista de música reseta ao mudar de página
 //TODO lista virtual
 
 export default function MusicLibrary() {
+  const playerAPI = usePlayerAPI();
   const [tracks, setTracks] = useState<TrackModel[]>();
 
   useEffect(() => {
@@ -24,10 +24,8 @@ export default function MusicLibrary() {
   }, []);
 
   const playTracks = () => {
-    console.log('add to playlist');
-
-    tracks?.map((track) => queue.add(track));
-    player.startFromQueue();
+    if (tracks)
+      playerAPI.start(tracks);
   };
 
   return (
