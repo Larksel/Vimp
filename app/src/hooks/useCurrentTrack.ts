8ff5318f7156cdba3519e02f0a8cf50fdec353ea
Larksel <lemuelgames@outlook.com>
2024-05-30 03:21:52 -0300

    :sparkles: feat: useCurrentTrack retorna uma track vazia

--- app/src/hooks/useCurrentTrack.ts
+++ app/src/hooks/useCurrentTrack.ts
@@ -1,12 +1,23 @@
 import usePlayerStore from "@/stores/usePlayerStore";
-import { TrackModel } from "../../shared/types/vimp";
+import { Track, TrackModel } from "../../shared/types/vimp";
 
-export default function useCurrentTrack(): TrackModel | null {
+export default function useCurrentTrack(): TrackModel | Track {
   return usePlayerStore((state) => {
     if (state.queue.length > 0 && state.queuePosition !== null) {
       return state.queue[state.queuePosition];
     }
 
-    return null;
+    return {
+      title: '',
+      album: '',
+      artist: [''],
+      genre: [''],
+      duration: 0,
+      playCount: 0,
+      favorite: false,
+      lastPlayed: null,
+      path: '',
+      cover: '',
+    };
   })
 }
