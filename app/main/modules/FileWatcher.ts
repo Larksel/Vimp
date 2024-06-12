bf895927a4b5b315c9d847b4f69c6f99b84828c1
Larksel <lemuelgames@outlook.com>
2024-06-11 21:30:48 -0300

    :sparkles: feat: Arquivos não suportados agora são corretamente ignorados

--- app/main/modules/FileWatcher.ts
+++ app/main/modules/FileWatcher.ts
@@ -1,12 +1,9 @@
-import Module from "./BaseModule";
 import type Store from 'electron-store';
-import { Config } from "../../shared/types/vimp";
 import chokidar from 'chokidar';
-import globals from "../../shared/lib/globals";
-
-const ignoredExtensions = globals.SUPPORTED_TRACKS_EXTENSIONS.map(ext => `**/*${ext}`).map(ext => `!${ext}`);
-
-ignoredExtensions.push('**/*');
+import path from 'path';
+import Module from './BaseModule';
+import { Config } from '../../shared/types/vimp';
+import globals from '../../shared/lib/globals';
 
 export default class FileWatcher extends Module {
   protected config: Store<Config>;
@@ -18,25 +15,40 @@ export default class FileWatcher extends Module {
   }
 
   async load() {
-    const watcher = chokidar.watch(this.config.get('musicFolders'), {
+    const filesToWatch = this.getFilesToWatch();
+    const watcher = chokidar.watch(filesToWatch, {
       persistent: true,
       awaitWriteFinish: true,
-      ignored: ignoredExtensions
-    })
-    // TODO fazer o mais facil e depois melhorar
-
+    });
     // TODO eu deveria apenas verificar ao iniciar e em resposta a um input do usuario?
-    
+
     // TODO verificar se ja existe no banco. Se não existir, adicione
     // TODO como saber se um arquivo foi movido ou renomeado?
     // TODO onde ficarão os handlers para esses casos?
-    // TODO filtrar para incluir apenas os arquivos suportados (globals.SUPPORTED_TRACKS_EXTENSIONS)
-    // TODO ignorar arquivos desconhecidos
     watcher
-      .on('add', path => console.log(`File ${path} has been added`))
-      .on('change', path => console.log(`File ${path} has been changed`))
-      .on('unlink', path => console.log(`File ${path} has been removed`))
-      .on('error', error => console.log(`Watcher error ${error}`))
-      .on('ready', () => console.log('Initial scan complete. Ready for changes.'));
+      .on('add', (path) => console.log(`File ${path} has been added`))
+      .on('change', (path) => console.log(`File ${path} has been changed`))
+      .on('unlink', (path) => console.log(`File ${path} has been removed`))
+      .on('error', (error) => console.log(`Watcher error ${error}`))
+      .on('ready', () =>
+        console.log('Initial scan complete. Ready for changes.'),
+      );
+  }
+
+  getFilesToWatch() {
+    const musicFolders = this.config.get('musicFolders');
+    const normalizedFolders = musicFolders.map((folder) =>
+      path.normalize(folder),
+    );
+
+    const filesToWatch: string[] = [];
+    
+    normalizedFolders.map((folder) =>
+      globals.SUPPORTED_TRACKS_EXTENSIONS.map((ext) =>
+        filesToWatch.push(`${folder}/**/*${ext}`),
+      ),
+    );
+
+    return filesToWatch;
   }
 }
