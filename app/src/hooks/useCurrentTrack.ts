98b0f01d0f9a7eca70015f91b8ba7dcd30e4a377
Larksel <lemuelgames@outlook.com>
2024-06-13 01:27:33 -0300

    :boom: fix: Revertendo ":construction: adaptar para track nulo"
    This reverts commit 659b17f2e021c51549384c764dad56ea0d4b18ed.

--- app/src/hooks/useCurrentTrack.ts
+++ app/src/hooks/useCurrentTrack.ts
@@ -2,12 +2,26 @@ import usePlayerStore from "@/stores/usePlayerStore";
 import { TrackModel } from "../../shared/types/vimp";
 
 
-export default function useCurrentTrack(): TrackModel | null {
+//TODO retornar null
+export default function useCurrentTrack(): TrackModel {
   return usePlayerStore((state) => {
     if (state.queue.length > 0 && state.queuePosition !== null) {
       return state.queue[state.queuePosition];
     }
 
-    return null;
+    return {
+      title: '',
+      album: '',
+      artist: [''],
+      genre: [''],
+      duration: 0,
+      playCount: 0,
+      favorite: false,
+      lastPlayed: null,
+      path: '',
+      cover: '',
+      _id: '',
+      _rev: '',
+    };
   })
 }
