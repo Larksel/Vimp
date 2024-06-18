a1756ddddc35fb7516652801da8f1b71c36db3fa
Larksel <lemuelgames@outlook.com>
2024-06-18 02:18:08 -0300

    :lipstick: feat: alinhamento da TrackList corrigido

--- app/src/componentes/TrackList/TrackList.tsx
+++ app/src/componentes/TrackList/TrackList.tsx
@@ -11,7 +11,7 @@ export default function TrackList(props: TrackListProps) {
   const { queue, queuePosition } = props;
 
   return (
-    <div className='flex h-full flex-col items-center justify-center'>
+    <div className='flex h-full flex-col items-center justify-start'>
       <div className='grid h-9 w-full grid-cols-[16px,6fr,4fr,3fr,1fr] gap-4 px-4 text-sm text-neutral-300'>
         <span className='flex items-center justify-center'>#</span>
         <span className='flex items-center'>Titulo</span>
