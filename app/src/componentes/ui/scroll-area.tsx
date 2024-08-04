ddcb95477000de0e8d907a89cd7423dbf8deaae3
Larksel <lemuelgames@outlook.com>
2024-08-03 21:21:29 -0300

    :lipstick: feat: cor da barra de rolagem sólida

--- app/src/componentes/ui/scroll-area.tsx
+++ app/src/componentes/ui/scroll-area.tsx
@@ -36,7 +36,7 @@ const ScrollBar = forwardRef<
     )}
     {...props}
   >
-    <ScrollAreaPrimitive.ScrollAreaThumb className='relative flex-1 rounded-full bg-neutral-200/20' />
+    <ScrollAreaPrimitive.ScrollAreaThumb className='relative flex-1 rounded-full bg-neutral-600' />
   </ScrollAreaPrimitive.ScrollAreaScrollbar>
 ));
 ScrollBar.displayName = ScrollAreaPrimitive.ScrollAreaScrollbar.displayName;
