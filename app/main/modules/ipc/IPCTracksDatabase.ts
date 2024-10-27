0cb02a99f7ed0125f71674080e79c9d027f1b35e
Larksel <lemuelgames@outlook.com>
2024-10-26 22:27:36 -0300

    :sparkles: feat: criação dos handlers IPC genéricos para banco de dados

--- app/main/modules/ipc/IPCTracksDatabase.ts
+++ app/main/modules/ipc/IPCTracksDatabase.ts
@@ -1,53 +1,29 @@
 import { ipcMain } from 'electron';
 import { IDBManager } from '@interfaces/modules/IDBManager';
 import { ITracksDatabase } from '@interfaces/databases/ITracksDatabase';
-import { Track, TrackModel } from '@shared/types/vimp';
+import { Track } from '@shared/types/vimp';
 
 import IPCChannels from '@shared/constants/IPCChannels';
-import BaseModule from '@modules/BaseModule';
+import BaseIPCDatabaseModule from './BaseIPCDatabaseModule';
 
-export default class IPCTracksDatabase extends BaseModule {
+export default class IPCTracksDatabase extends BaseIPCDatabaseModule<Track> {
   private readonly TracksDB: ITracksDatabase;
 
   constructor(dbManager: IDBManager) {
-    super();
+    super(dbManager.getTracksDB(), {
+      GET_ALL: IPCChannels.TRACKSDB_GET_ALL,
+      GET_BY_ID: IPCChannels.TRACKSDB_GET_BY_ID,
+      CREATE: IPCChannels.TRACKSDB_CREATE,
+      UPDATE: IPCChannels.TRACKSDB_UPDATE,
+      DELETE: IPCChannels.TRACKSDB_DELETE,
+      CLEAR: IPCChannels.TRACKSDB_CLEAR,
+    });
 
     this.TracksDB = dbManager.getTracksDB();
   }
 
   protected async load() {
-    // * CRUD operations
-    ipcMain.handle(IPCChannels.TRACKSDB_GET_ALL, async () => {
-      return this.TracksDB.getAll();
-    });
-
-    ipcMain.handle(
-      IPCChannels.TRACKSDB_GET_BY_ID,
-      async (_, trackID: string) => {
-        return this.TracksDB.getById(trackID);
-      },
-    );
-
-    ipcMain.handle(
-      IPCChannels.TRACKSDB_CREATE,
-      async (_, tracks: Track | Track[]) => {
-        await this.TracksDB.create(tracks);
-      },
-    );
-
-    ipcMain.handle(
-      IPCChannels.TRACKSDB_UPDATE,
-      async (_, tracks: TrackModel | TrackModel[]) => {
-        return this.TracksDB.update(tracks);
-      },
-    );
-
-    ipcMain.handle(
-      IPCChannels.TRACKSDB_DELETE,
-      async (_, tracks: TrackModel | TrackModel[]) => {
-        await this.TracksDB.delete(tracks);
-      },
-    );
+    this.createGenericIPCHandlers();
 
     ipcMain.handle(
       IPCChannels.TRACKSDB_GET_BY_PATH,
@@ -55,8 +31,6 @@ export default class IPCTracksDatabase extends BaseModule {
         return this.TracksDB.getByPath(trackPath);
       },
     );
-
-    // * Features
 
     ipcMain.handle(
       IPCChannels.TRACKSDB_INCREMENT_PLAY_COUNT,
@@ -78,11 +52,5 @@ export default class IPCTracksDatabase extends BaseModule {
         await this.TracksDB.updateLastPlayed(trackID);
       },
     );
-
-    // * Helpers
-
-    ipcMain.handle(IPCChannels.TRACKSDB_CLEAR, async () => {
-      await this.TracksDB.clear();
-    });
   }
 }
