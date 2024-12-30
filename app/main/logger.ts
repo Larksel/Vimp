afbb988ee4c67bfdca8b6d4c382d8c2e7c02606e
Larksel <lemuelgames@outlook.com>
2024-12-30 14:45:05 -0300

    :wrench: chore: Formato do nome do arquivo de log corrigido

--- app/main/logger.ts
+++ app/main/logger.ts
@@ -8,14 +8,11 @@ export default function setupLogger(isDebug: boolean) {
       year: 'numeric',
       month: '2-digit',
       day: '2-digit',
-      hour: '2-digit',
-      minute: '2-digit',
-      second: '2-digit',
       hour12: false,
     })
-    .replace(/\//g, '-')
-    .replace(',', '')
-    .replace(/:/g, '-');
+    .split('/')
+    .reverse()
+    .join('-');
 
   // File
   log.transports.file.fileName = `${formattedDate}.log`;
