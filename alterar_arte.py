36029c04d47781174e3d03d7791fc8241d028619
Larksel <lemuelgames@outlook.com>
2025-01-01 19:48:31 -0300

    :recycle: refactor: Pequenas correções ao alterar_arte

--- alterar_arte.py
+++ alterar_arte.py
@@ -1,32 +1,31 @@
-from app.main.downloader import Downloader
-from app.main.managers import FileManager
+from app.main.python.downloader import Downloader
+from app.main.python.managers import FileManager
 from pathlib import Path
 
 
 def main():
     while True:
-        url = input("Link do vídeo \n -> ")
+        url = input("Link do vídeo \n-> ")
 
-        if url != '':
+        if url != "":
             downloader = Downloader(url)
             print(downloader.title)
         else:
-            print('Nenhum valor inserido')
+            print("Nenhum valor inserido. Encerrando...")
             break
-        
-        audiofile = input("Arquivo a ser alterado \n -> ").replace('"', '')
+
+        audiofile = input("Arquivo a ser alterado \n-> ").replace('"', "")
 
         if audiofile != "":
             titulo = Path(audiofile).stem
-            
+
             downloader.get_thumbnail()
 
             FileManager.set_audio_metadata(
-                audiofile=audiofile,
-                thumbimg=downloader.thumbnail_path,
-                titulo=titulo
+                audiofile=audiofile, thumbimg=downloader.thumbnail_path, titulo=titulo
             )
         else:
-            print('Valor inválido')
+            print("Valor inválido")
+
 
 main()
