8c2e394deceb15e8aef2a01c11e4b97f32c2520c
Larksel <lemuelgames@outlook.com>
2025-01-07 04:36:38 -0300

    :sparkles: feat: PlaylistAPI criada

--- app/renderer/stores/useLibraryStore.ts
+++ app/renderer/stores/useLibraryStore.ts
@@ -15,6 +15,9 @@ interface LibraryState {
     setPlaylists: (playlists: PlaylistModel[]) => void;
     getTracksFromIDs: (trackIDs?: string[]) => TrackModel[];
   };
+  playlistApi: {
+    toggleFavorite: (_id: string) => Promise<void>;
+  };
 }
 
 const useLibraryStore = createLibraryStore<LibraryState>((set, get) => ({
@@ -59,12 +62,45 @@ const useLibraryStore = createLibraryStore<LibraryState>((set, get) => ({
         .filter((track) => !!track);
     },
   },
+  playlistApi: {
+    toggleFavorite: async (_id) => {
+      if (!_id && _id === '') return;
+
+      const { contents } = get();
+      const { playlists } = contents;
+      const index = playlists.findIndex((playlist) => playlist._id === _id);
+
+      if (index > -1) {
+        const playlist = playlists[index];
+        await window.VimpAPI.playlistsDB.updateFavorite(playlist._id);
+
+        log.debug(`[LibraryStore] Favorited playlist: ${playlist.title}`);
+
+        const updatedPlaylists = [...playlists];
+        updatedPlaylists[index] = {
+          ...playlist,
+          favorite: !playlist.favorite,
+        };
+
+        set({
+          contents: {
+            ...contents,
+            playlists: updatedPlaylists,
+          },
+        });
+      }
+    },
+  },
 }));
 
 export default useLibraryStore;
 
 export function useLibraryAPI() {
   return useLibraryStore((state) => state.api);
+}
+
+export function usePlaylistAPI() {
+  return useLibraryStore((state) => state.playlistApi);
 }
 
 function createLibraryStore<T extends LibraryState>(store: StateCreator<T>) {
