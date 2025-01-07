5c3fa04698bf99e0ba56bf424826d706cf95c95b
Larksel <lemuelgames@outlook.com>
2025-01-07 03:28:45 -0300

    :recycle: refactor: melhoria na src da imagem da ExpandedView

--- app/renderer/components/PlaybackConsole/ExpandedView.tsx
+++ app/renderer/components/PlaybackConsole/ExpandedView.tsx
@@ -13,12 +13,12 @@ export default function ExpandedView({ visible }: ExpandedViewProps) {
       className={`${visible ? 'top-0 opacity-100' : 'top-full opacity-0'} absolute bottom-[88px] left-0 right-0 bg-black transition-all`}
     >
       <img
-        src={track ? track.cover || placeholder : placeholder}
+        src={track?.cover ?? placeholder}
         alt=''
         className='absolute inset-0 size-full object-cover opacity-30 blur'
       />
       <img
-        src={track ? track.cover || placeholder : placeholder}
+        src={track?.cover ?? placeholder}
         alt=''
         className={`absolute left-4 size-64 rounded-lg border border-black/30 object-cover shadow-md transition-all ${visible ? 'visible bottom-4' : 'invisible bottom-0'}`}
       />
