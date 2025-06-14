0d2d6981ef5ac012b91edd5e19b196de59373902
Larksel <lemuelgames@outlook.com>
2025-06-14 19:25:01 -0300

    :recycle: refactor: Suavização do analyser reduzida

--- app/renderer/features/player.ts
+++ app/renderer/features/player.ts
@@ -31,7 +31,7 @@ class Player {
     this.audioSource.connect(this.analyser);
     this.analyser.connect(this.audioCtx.destination);
     this.analyser.fftSize = 4096;
-    this.analyser.smoothingTimeConstant = 0.8;
+    this.analyser.smoothingTimeConstant = 0.65;
     this.analyser.maxDecibels = -10;
     this.analyser.minDecibels = -100;
 
