f8123a5ec408365616617b15d43f2867090a5868
Larksel <lemuelgames@outlook.com>
2025-07-04 02:51:16 -0300

    :sparkles: feat: Menu de ordenação básico

--- app/renderer/components/SideBar/ListHeader.tsx
+++ app/renderer/components/SideBar/ListHeader.tsx
@@ -12,6 +12,7 @@ import { createRendererLogger } from '@render-utils/logger';
 import { Playlist } from '@shared/types/vimp';
 import { createGenericPlaylist } from '@shared/utils/utils';
 import { PlaylistPersistenceService } from '@features/data';
+import { RadioGroup, RadioGroupItem } from '@components/common/radio-group';
 
 const logger = createRendererLogger('SideBar');
 
@@ -23,6 +24,7 @@ interface ListHeaderProps {
 export default function ListHeader(props: ListHeaderProps) {
   const { collapsed, searchHandler } = props;
   const [popoverNewPlaylist, setPopoverNewPlaylist] = useState(false);
+  const [popoverOptions, setPopoverOptions] = useState(false);
   const [playlistName, setPlaylistName] = useState('');
 
   const openClosePopoverNewPlaylist = () => {
@@ -31,6 +33,10 @@ export default function ListHeader(props: ListHeaderProps) {
     }
 
     setPopoverNewPlaylist(!popoverNewPlaylist);
+  };
+
+  const openClosePopoverOptions = () => {
+    setPopoverOptions(!popoverOptions);
   };
 
   const createPlaylist = async (e: FormEvent<HTMLFormElement>) => {
@@ -55,7 +61,7 @@ export default function ListHeader(props: ListHeaderProps) {
           <Button
             variant='surface'
             onClick={openClosePopoverNewPlaylist}
-            className={`text-text-secondary hover:text-text-primary flex aspect-square rounded-full p-0 transition-all ${collapsed ? 'mx-1' : ''}`}
+            className={`text-text-secondary hover:text-text-primary flex aspect-square rounded-full p-0 transition-all ${collapsed && 'mx-1'}`}
           >
             <Plus size={20} />
           </Button>
@@ -97,12 +103,60 @@ export default function ListHeader(props: ListHeaderProps) {
         />
       </div>
 
-      <Button
-        variant='surface'
-        className={`text-text-secondary hover:text-text-primary flex aspect-square rounded-full p-0 transition-all ${collapsed ? 'w-0 overflow-clip' : ''}`}
-      >
-        <ListBullets size={20} />
-      </Button>
+      <Popover open={popoverOptions} modal>
+        <PopoverTrigger asChild>
+          <Button
+            variant='surface'
+            onClick={openClosePopoverOptions}
+            className={`text-text-secondary hover:text-text-primary flex aspect-square rounded-full p-0 transition-all ${collapsed && 'w-0 overflow-clip'}`}
+          >
+            <ListBullets size={20} />
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent
+          align='center'
+          side='left'
+          onEscapeKeyDown={openClosePopoverOptions}
+          onInteractOutside={openClosePopoverOptions}
+        >
+          <div className='flex flex-col'>
+            <h6 className='font-semibold'>Sort By</h6>
+            <RadioGroup defaultValue='title'>
+              <div className='flex items-center gap-3'>
+                <RadioGroupItem value='title' id='title' />
+                <label htmlFor='title'>Title</label>
+              </div>
+              <div className='flex items-center gap-3'>
+                <RadioGroupItem value='artist' id='artist' />
+                <label htmlFor='artist'>Artist</label>
+              </div>
+              <div className='flex items-center gap-3'>
+                <RadioGroupItem value='album' id='album' />
+                <label htmlFor='album'>Album</label>
+              </div>
+              <div className='flex items-center gap-3'>
+                <RadioGroupItem value='dateAdded' id='dateAdded' />
+                <label htmlFor='dateAdded'>Date Added</label>
+              </div>
+              <div className='flex items-center gap-3'>
+                <RadioGroupItem value='duration' id='duration' />
+                <label htmlFor='duration'>Duration</label>
+              </div>
+            </RadioGroup>
+            <h6 className='font-semibold'>Direction</h6>
+            <RadioGroup defaultValue='ascending'>
+              <div className='flex items-center gap-3'>
+                <RadioGroupItem value='ascending' id='ascending' />
+                <label htmlFor='ascending'>Ascending</label>
+              </div>
+              <div className='flex items-center gap-3'>
+                <RadioGroupItem value='descending' id='descending' />
+                <label htmlFor='descending'>Descending</label>
+              </div>
+            </RadioGroup>
+          </div>
+        </PopoverContent>
+      </Popover>
     </div>
   );
 }
