54bef61884d27178e868d3a69392cc2407fce238
Larksel <lemuelgames@outlook.com>
2025-07-21 18:03:38 -0300

    :sparkles: feat: (Python) endpoint de download funcional

--- api/vimp_api.py
+++ api/vimp_api.py
@@ -1,37 +1,46 @@
 from os.path import exists
-from fastapi import FastAPI, HTTPException, Response
+from fastapi import FastAPI, Response
 from fastapi.responses import FileResponse
+from urllib.parse import quote
+
+from modules.managers import DirManager
+from modules.downloader import Downloader
 
 
 class VimpAPI:
     def __init__(self) -> None:
         self.app = FastAPI(title="VimpAPI")
+        self.dirmanager = DirManager()
         self._setup_routes()
 
     def _setup_routes(self):
         """Configura todos os endpoints da API."""
 
-        @self.app.get("/download_track", response_class=FileResponse)
+        @self.app.get("/download_track/", response_class=FileResponse)
         async def download_track(link: str, response: Response):
             return await self._handle_download_track(link, response)
 
     async def _handle_download_track(self, link: str, response: Response):
-        # TODO Descobrir o nome do arquivo pelo titulo do video
-        filepath = ""
-        filename = ""
+        downloader = Downloader(link)
+        filename = f"{downloader.formatted_title}.mp3"
+        filepath = self.dirmanager.MUSIC_FOLDER + filename
 
-        # TODO Verificar se o arquivo ja existe
-        # TODO Se existir, envia como resposta
-        # TODO Senão baixa para o computador e envia
-        # ? FUTURO: Devo baixar para uma pasta temporária?
-        if not exists(filepath):
-            raise HTTPException(status_code=404, detail="Arquivo não encontrado")
+        print(f"Filename: {filename}")
 
         # Definição dos cabeçalhos da resposta
         # Evita que o navegador faça cache da requisição e trave a comunicação
         response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate"
         response.headers["Pragma"] = "no-cache"
         response.headers["Expires"] = "0"
-        response.headers["Content-Disposition"] = f"attachment; filename={filename}"
+        encoded_filename = quote(filename.encode("utf-8"))
+        response.headers["Content-Disposition"] = (
+            f"attachment; filename*=UTF-8''{encoded_filename}"
+        )
+
+        if not exists(filepath):
+            print("Música não encontrada. Baixando...")
+            downloader.get_music()
+
+        print(f"Enviando: {filepath}")
 
         return filepath
