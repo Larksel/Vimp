11e76771ae1342bfe19f68c229efcfe0a298ae1d
Larksel <lemuelgames@outlook.com>
2025-08-29 22:29:25 -0300

    :lipstick: feat: Protótipo do Downloader melhorado

--- app/renderer/views/DownloaderView.tsx
+++ app/renderer/views/DownloaderView.tsx
@@ -8,26 +8,32 @@ export default function DownloaderView() {
   const track = useCurrentTrack(); // Placeholder temporário
 
   return (
-    <div className='flex flex-col gap-4 px-4 pb-4'>
+    <div className='flex flex-col items-center gap-4 px-4 pb-4'>
       <SearchBox
         name='download-link'
         canChangeVisibility={false}
         onSearch={(search) => console.log(search)}
-        className='rounded-lg'
+        className='max-w-[50%] rounded-lg'
       />
-      <div className='flex h-full w-[50%] flex-col justify-center gap-4'>
-        <img
-          src={track?.cover ?? placeholder}
-          alt=''
-          className='aspect-video h-full rounded-lg object-cover'
-        />
-        <div className='flex h-full flex-col justify-between'>
+      <div className='flex h-full w-[40%] flex-col justify-center gap-4 self-start p-4'>
+        <div className='relative overflow-clip rounded-lg'>
+          <img
+            src={track?.cover ?? placeholder}
+            alt=''
+            className='aspect-video w-full object-cover'
+          />
+          <p className='bg-glass-base absolute right-0 bottom-0 rounded-tl-sm px-1 text-sm'>
+            {formatDuration(track?.duration)}
+          </p>
+        </div>
+        <div className='flex h-full flex-col gap-8'>
           {track ? (
             <>
               <div>
-                <h6 className='truncate'>{track?.title}</h6>
-                <p className='truncate'>{track?.artist}</p>
-                <p>Duração: {formatDuration(track?.duration)}</p>
+                <h6 className='text-text-primary truncate text-xl font-bold'>
+                  {track?.title}
+                </h6>
+                <p className='text-text-secondary truncate'>{track?.artist}</p>
               </div>
               <Button className='bg-accent' variant={'filled'}>
                 Download
