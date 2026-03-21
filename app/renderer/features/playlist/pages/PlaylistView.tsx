931f965a8cf499b72a2d14c3dd27ff8b3ff01305
Larksel <lemuelgames@outlook.com>
2026-03-21 01:34:42 -0300

    :lipstick: feat: Substituído animação gradual do header de PlaylistView para um toggle

--- app/renderer/features/playlist/pages/PlaylistView.tsx
+++ app/renderer/features/playlist/pages/PlaylistView.tsx
@@ -1,8 +1,9 @@
-import { useState, useMemo, useRef } from 'react';
+import { useState, useRef } from 'react';
 import TrackList from '@renderer/components/TrackList';
 import placeholder from '@renderer/assets/images/placeholder.png';
 import { HeartStraightIcon } from '@phosphor-icons/react/dist/csr/HeartStraight';
 import { PlayIcon } from '@phosphor-icons/react/dist/csr/Play';
+import { CaretDownIcon } from '@phosphor-icons/react/dist/csr/CaretDown';
 import useLibraryStore from '@renderer/stores/useLibraryStore';
 import { usePlayerAPI } from '@renderer/stores/usePlayerStore';
 import { useParams } from 'react-router-dom';
@@ -15,11 +16,11 @@ import { usePlaylistAPI } from '@renderer/stores/usePlaylistStore';
 
 export default function PlaylistView() {
   const { id } = useParams();
+  const [isHeaderCollapsed, setIsHeaderCollapsed] = useState(true);
   const loading = useLibraryStore((state) => state.loading);
   const playerAPI = usePlayerAPI();
   const playlistAPI = usePlaylistAPI();
   const loaderData = usePlaylistLoader(id);
-  const [scroll, setScroll] = useState(0);
   const bgImageRef = useRef<HTMLImageElement>(null);
 
   useAudioAnimation([bgImageRef], (audioData) => {
@@ -31,23 +32,6 @@ export default function PlaylistView() {
       filter: `brightness(${brightness}) blur(${blur}px)`,
     };
   });
-
-  const scrollProgress = useMemo(() => {
-    const scrollThreshold = 147; // Magic number, for now...
-    const progress = Math.min(1, scroll / scrollThreshold);
-    return 1 - Math.pow(1 - progress, 3); // ease-out
-  }, [scroll]);
-
-  const headerHeight = useMemo(() => {
-    const maxHeight = 260;
-    const minHeight = 120;
-    const height = maxHeight - scrollProgress * (maxHeight - minHeight);
-    return Math.max(minHeight, Math.min(maxHeight, height));
-  }, [scrollProgress]);
-
-  const handleScroll = (scrollTop: number) => {
-    setScroll(scrollTop);
-  };
 
   if (loading.playlists || loading.tracks) {
     return (
@@ -92,37 +76,51 @@ export default function PlaylistView() {
   };
 
   return (
-    <div className='z-1 flex flex-col'>
-      <div
-        className='z-1 flex gap-4 px-4 pb-4 transition-all duration-500 ease-out'
-        style={{
-          height: `${headerHeight}px`,
-        }}
+    <div className='flex flex-col'>
+      <img
+        ref={bgImageRef}
+        src={playlist.cover ?? placeholder}
+        alt=''
+        className='absolute inset-0 h-full w-full object-cover blur-lg brightness-[0.2]'
+      />
+      <header
+        className={`relative z-10 flex gap-4 p-4 transition-all duration-500 ${isHeaderCollapsed ? 'h-30' : 'h-65'}`}
       >
+        <Button
+          variant={'glass'}
+          className='absolute right-1/2 bottom-0 z-20 size-8 rounded-full bg-transparent p-0 transition-transform hover:bg-transparent active:bg-transparent'
+          onClick={() => setIsHeaderCollapsed(!isHeaderCollapsed)}
+        >
+          <CaretDownIcon
+            size={20}
+            weight='bold'
+            className={`transition-transform ${!isHeaderCollapsed && 'rotate-180'}`}
+          />
+        </Button>
         <img
           src={playlist.cover ?? placeholder}
           alt='playlist cover'
-          className='z-1 aspect-square h-full rounded-lg object-cover shadow-md select-none'
+          className='aspect-square h-full rounded-lg object-cover shadow-md select-none'
         />
         <div
-          className={`relative flex w-full flex-col justify-center transition-all ${scrollProgress < 0.75 ? 'gap-2' : 'gap-0'}`}
+          className={`relative flex w-full flex-col justify-center transition-all ${isHeaderCollapsed ? 'gap-0' : 'gap-2'}`}
         >
           <InfoText
             variant={'secondary'}
-            className={`text-sm transition-all ${scrollProgress > 0.75 ? 'h-0 opacity-0' : 'opacity-100'}`}
+            className={`text-sm transition-all ${isHeaderCollapsed ? 'h-0 opacity-0' : 'opacity-100'}`}
           >
             Playlist
           </InfoText>
 
           <InfoText
             variant={'primary'}
-            className={`w-full truncate font-bold transition-all duration-500 ${scrollProgress > 0.75 ? 'text-3xl' : 'text-5xl'}`}
+            className={`w-full truncate font-bold transition-all duration-500 ${isHeaderCollapsed ? 'text-3xl' : 'text-5xl'}`}
           >
             {playlist.title}
           </InfoText>
 
           <InfoText
-            variant={scrollProgress > 0.75 ? 'secondary' : 'primary'}
+            variant={isHeaderCollapsed ? 'secondary' : 'primary'}
             className='text-sm transition-colors'
           >
             {`${tracks.length} tracks - ${totalDuration()}`}
@@ -131,7 +129,7 @@ export default function PlaylistView() {
           {playlist.description && (
             <InfoText
               variant={'secondary'}
-              className={`line-clamp-2 w-full pr-24 text-sm whitespace-normal transition-all duration-300 sm:line-clamp-3 md:line-clamp-4 ${scrollProgress > 0.75 ? 'h-0 opacity-0' : 'opacity-100'}`}
+              className={`line-clamp-2 w-full pr-24 text-sm whitespace-normal transition-all duration-500 sm:line-clamp-3 md:line-clamp-4 ${isHeaderCollapsed ? 'h-0 opacity-0' : 'opacity-100'}`}
             >
               {playlist.description}
             </InfoText>
@@ -139,7 +137,7 @@ export default function PlaylistView() {
 
           <div
             className={`flex gap-2 transition-all duration-500 ${
-              scrollProgress > 0.75 ? 'absolute right-0' : 'mt-auto'
+              isHeaderCollapsed ? 'absolute right-0' : 'mt-auto'
             }`}
           >
             <Button
@@ -163,21 +161,14 @@ export default function PlaylistView() {
             </Button>
           </div>
         </div>
-      </div>
-      <div className='z-1 h-full'>
+      </header>
+      <div className='z-10 min-h-0 w-full flex-1'>
         <TrackList
           items={tracks}
           onItemClick={handleItemClick}
           onItemMove={handleItemMove}
-          onScroll={handleScroll}
         />
       </div>
-      <img
-        ref={bgImageRef}
-        src={playlist.cover ?? placeholder}
-        alt=''
-        className='absolute inset-0 z-0 h-full w-full object-cover blur-lg brightness-[0.2]'
-      />
     </div>
   );
 }
